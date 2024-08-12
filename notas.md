f6a9c99d92f183b0a5b3bdb528bbc184cf6d389a
Danilo Espinoza Villalta <despinoza@prides.net>
2024-08-11 21:37:58 -0600

    finnish session_04 11082024

---
+++ notas.md
@@ -0,0 +1,27 @@
+# Crear proyecto react
+https://create-react-app.dev/
+
+https://github.com/Klerith/react-intro-javascript/blob/master/src/bases/07-deses-arr.js
+
+# Comandos
+Crear:
+ npx create-react-app my-app 
+Crear con Vite y doc:
+https://vitejs.dev/
+
+# NOTAS 
+BUSCAR SIEMPRE CON RESPECTO A !NOTA
+
+
+# Snippets 
+rafc
+
+# Imports
+import PropTypes from 'prop-types'
+
+# Components events
+https://es.react.dev/reference/react-dom/components/common#react-event-object
+
+# Hooks
+https://es.react.dev/reference/react/hooks
+--useState para enviar y refrescar
--- seccion_04/task1-counter-app/src/components/CounterComponent.jsx
+++ seccion_04/task1-counter-app/src/components/CounterComponent.jsx
@@ -1,20 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const CounterComponent = ({counter}) => {
+
+const CounterComponent = ({ counter }) => {
+
+  console.log("render nuevo")
+  // !NOTA con el cambio de estados el componente se vuelve a cargar por cada cambio, cuidao con esto!
+  function clickEvent(event, counter2) {
+    console.log("🚀 ~ clickEvent ~ params:", counter)
+    console.log("🚀 ~ clickEvent ~ event:", event)
+    //No funciona porque se utiliza con hooks
+    counter = counter++;
+  }
+
+  function clickEventHook(event) {
+    // setCounterValue(counterValue + 1)
+    // !NOTA ENVIAR VALOR POR MEDIO DE FUNCIÓN, ambas funcionan igual
+    setCounterValue((x) => x + 1)
+  }
+
+
+  function sum(params) {
+    setCounterValue((x) => x + 1)
+  }
+
+  function min(params) {
+    setCounterValue((c) => c - 1)
+  }
+
+  function reset(params) {
+    setCounterValue((x) => counter)
+  }
+
+
+  // constantes para no modificar el valor, solo con el argument, toma el valor inicial del prop
+  const [counterValue, setCounterValue] = useState(counter);
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { counter } </h2>
+      <h6>El valor que ahora se controla es el del state no el del prop</h6>
+      <h2> Valor prop {counter} </h2>
+
+      <h2> Valor useState {counterValue} </h2>
+      {/* El valor que ahora se controla es el del state no el del prop */}
+
+      <button className='button' onClick={(event) => clickEvent(event, counter)}> Aumentar Console</button>
+
+      <button className='button' onClick={(event) => clickEventHook(event)}> Aumentar Hook</button>
+
+
+      <div className='divContainer'>
+        <button className='button' onClick={(event) => sum(event)}> +1</button>
+
+        <button className='button' onClick={(event) => min(event)}> -1</button>
+
+        <button className='button' onClick={(event) => reset(event)}> Reset</button>
+      </div>
     </>
   )
 }
 
 
+
 CounterComponent.propTypes = {
-    // isRequired permite para saber si es requerido o no un campo
-    counter: PropTypes.number.isRequired
+  // isRequired permite para saber si es requerido o no un campo
+  counter: PropTypes.number.isRequired
 }
-  
+
 
 export default CounterComponent
